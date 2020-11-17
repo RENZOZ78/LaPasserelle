156c3e14d85d054f1cb844384a92988e0c8f68bc
didier <didierbap@gmail.com>
2020-11-17 19:07:15 +0100

    CORRECTION BUGs CSS

--- inc/section.php
+++ inc/section.php
@@ -4,50 +4,24 @@
             <h2 class="articles_title">Articles</h2>
             <div class="articles_items">
 
+
+
                 <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article1.php?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="article1"
-                   style="background: url('public/img/img1.jpg');  background-size: cover;">
+                       class="article"
+                       style="background: url('public/img/img1.jpg');  background-size: cover;">
                     <div class="article_filter"></div>
                     <div class="article_name">demarquez vous avec un style original</div>
                     <div class="article_icon"><i class="fa fa-play"></i></div>
                 </a>
                 <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article2.html?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="articl2"
+                   class="article"
                    style="background: url('public/img/img2.jpg');  background-size: cover;">
                     <div class="article_filter"></div>
                     <div class="article_name">Les vêtements dont vous avez toujours révé</div>
                     <div class="article_icon"><i class="fa fa-play"></i></div>
                 </a>
                 <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article3.html?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="article3"
-                   style="background: url('public/img/img3.jpg');  background-size: cover;">
-                    <div class="article_filter"></div>
-                    <div class="article_name">les meilleurs patisseries de la ville</div>
-                    <div class="article_icon"><i class="fa fa-play"></i></div>
-                </a>
-                <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article3.html?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                       class="article4"
-                       style="background: url('public/img/img3.jpg');  background-size: cover;">
-                    <div class="article_filter"></div>
-                    <div class="article_name">les meilleurs patisseries de la ville</div>
-                    <div class="article_icon"><i class="fa fa-play"></i></div>
-                </a>
-                <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article1.php?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="article5"
-                   style="background: url('public/img/img1.jpg');  background-size: cover;">
-                    <div class="article_filter"></div>
-                    <div class="article_name">demarquez vous avec un style original</div>
-                    <div class="article_icon"><i class="fa fa-play"></i></div>
-                </a>
-                <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article2.html?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="article6"
-                   style="background: url('public/img/img2.jpg');  background-size: cover;">
-                    <div class="article_filter"></div>
-                    <div class="article_name">Les vêtements dont vous avez toujours révé</div>
-                    <div class="article_icon"><i class="fa fa-play"></i></div>
-                </a>
-                <a href="http://localhost:63342/PROJET%20PERSO/LaPasserelle/article3.html?_ijt=i858qhni7c3sf8ou8i4cf2ir6l"
-                   class="article7"
+                   class="article"
                    style="background: url('public/img/img3.jpg');  background-size: cover;">
                     <div class="article_filter"></div>
                     <div class="article_name">les meilleurs patisseries de la ville</div>
@@ -88,6 +62,8 @@
                     <div class="article_name">les meilleurs patisseries de la ville</div>
                     <div class="article_icon"><i class="fa fa-play"></i></div>
                 </a>
+
+
 
             </div>
         </div>
